fc35fb6d7ebcab2b41107e7bb009c3b2eae2804b
DumboKim <rlawlsgus97@gmail.com>
2021-12-22 02:14:31 +0900

    성적 페이지로 이동

--- lib/web_page.dart
+++ lib/web_page.dart
@@ -21,6 +21,7 @@ class WebPage extends GetView {
         print(await _controller.currentUrl());
         webController.complete(_controller);
         // _controller.runJavascript(javaScriptString)
+        // _controller.runJavascriptReturningResult(javaScriptString)
       },
       initialUrl:
           'https://smartid.ssu.ac.kr/Symtra_sso/smln.asp?apiReturnUrl=https%3A%2F%2Fsaint.ssu.ac.kr%2FwebSSO%2Fsso.jsp',
@@ -36,11 +37,44 @@ class WebPage extends GetView {
         }
 
         if (url.contains('irj/portal')) {
+          // webController.future.then((value) async {
+          //   var string = await value.runJavascriptReturningResult('document.title');
+          //   print(string);
+          // });
           webController.future.then((value) async {
-            var string = await value.runJavascriptReturningResult(
-                "document.getElementsById('main_title').innerText;");
-            print('string : $string');
+            var saint = await value.runJavascript(
+                'document.getElementsByClassName("mob_gnb_list")[0].click();');
+
+            // var saint2 = await value.runJavascript(
+            //     'document.getElementsByClassName("c_m_nodeA")[43].click();');
+            Future.delayed(Duration(milliseconds: 300), () async {
+              await value
+                  .runJavascriptReturningResult(
+                      'document.getElementsByClassName("c_m_nodeA")[43].click();')
+                  .then((value2) async {
+                var string =
+                    await value.runJavascriptReturningResult('document.title');
+                print(string);
+              }).then((value3) async {
+                print(await value.runJavascriptReturningResult('document.getElementsByClassName("urNoUserSelect")[0].innerHTML'));
+              });
+            });
+            // .then((value2) async {
+
+            // });
+            // return value;
+            // }).then((value) => null);
+            // print('string : $string');
           });
+          // .then((value) async {
+          //   await value.runJavascript(
+          //       'document.getElementsByClassName("c_m_nodeA")[43].click();');
+          //   return value;
+          // }).then((value) async {
+          //   await value.runJavascript(
+          //       'document.getElementByName("WD0192-caption").click();');
+          //   return value;
+          // });
         }
 
         if (url.contains('irj/portal')) {
